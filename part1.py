62227f40ba9b762c7624434f67664c91b133a8fa
Wilfredo Joshua Robinson Moore <wilfredo-joshua.robinson-moore@eurecom.fr>
2023-11-01 13:37:03 -0500

    Added code for Part 1

---
+++ part1.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+np.set_printoptions(suppress=True)
+
+"""
+USER INPUTS
+"""
+
+# For our custom pose
+t1 = np.pi/2
+t2 = -np.pi/2
+t3 = 0
+t4 = 0
+t5 = 0
+t6 = 0
+
+# Define the robot properties
+theta= np.array([[t1],[t2],[t3],[t4],[t5],[t6]])
+
+a = np.array([
+    [0, 0, 0],
+    [0, 0, 284.8],
+    [0, 0, 694.8],
+    [0, 1, 0],
+    [0, 0, 1009.1],
+    [0, 1, 0]
+])
+
+rot = np.array([
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, -1, 0],
+    [0, 0, -1],
+    [0, -1, 0],
+    [0, 0, -1]
+])
+
+M = np.array([
+    [-1, 0, 0, 0],
+    [0, -1, 0, 1],
+    [0, 0, 1, 1176.50],
+    [0, 0, 0, 1]
+])
+
+jt = 'RRRRRR'
+
+
+"""
+PoE Function
+"""
+def FK_PoE(theta_val, a_val, rot_val, jt_val):
+    #Indentity Matrix
+    T=np.eye(4)
+    #Number of joints
+    n=max(theta_val.shape)
+    for ii in range(n-1, -1, -1):
+        if jt_val[ii] == 'R':
+            rot_hat = np.array([[0, -rot_val[ii,2], rot_val[ii, 1]],
+                                [rot_val[ii, 2], 0, -rot_val[ii,0]],
+                                [-rot_val[ii, 1], rot_val[ii, 0], 0]])
+            e_rot_hat=np.eye(3)+rot_hat*np.sin(theta_val[ii])+(rot_hat@rot_hat)*(1-np.cos(theta_val[ii]))
+        elif jt_val[ii] == 'P':
+            rot_hat = np.zeros((3, 3))
+            e_rot_hat=np.eye(3)
+        
+        if jt_val[ii] == 'R' and ii > 0:
+            Sv = np.cross(rot_val[ii, :], a_val[ii, :])
+            Sv = -Sv.T
+        elif jt_val[ii] == 'R' and ii == 0:
+            Sv = np.array([0, 0, 0])
+        elif jt_val[ii] == 'P':
+            Sv = a_val[ii, :]
+        t=(np.eye(3)*theta_val[ii]+(1-np.cos(theta_val[ii]))*rot_hat+(theta_val[ii]-np.sin(theta_val[ii]))*(rot_hat@rot_hat))@Sv
+        t_2=np.array([[t[0]], [t[1]], [t[2]]])
+
+        tee=np.hstack([e_rot_hat,t_2])
+
+        row=np.array([0, 0, 0, 1])
+        
+        e_zai=np.vstack([tee,row])
+        T=e_zai@T
+    return T
+
+"""
+Actually running the code!
+"""
+T= FK_PoE(theta, a, rot, jt)
+
+Tf = T @ M
+R = Tf[0:3, 0:3]
+p = Tf[0:3, 3]
+
+print(f"R = \n{R}")
+print(f"\np = \n{p}")
